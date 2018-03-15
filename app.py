dd4eabfb86df3157dbe6a1b6df0bfa2032b39740
Dean Flint <dean.p.flint@gmail.com>
2018-03-15 18:31:31 +0000

    functionalty added: add to scoreboard, view top 10 in scoreboard

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, redirect, render_template, request, url_for
+from operator import itemgetter
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -28,90 +29,157 @@ def get_all_attempts():
     with open("data/guesses.txt", "r") as incorrect_attempts:
         attempts = incorrect_attempts.readlines()
     return attempts
+    
+def num_of_attempts():
+    attempts = get_all_attempts()
+    return len(attempts);
+    
+def attempts_rem():
+    rem_attempts = 5 - num_of_attempts()
+    return rem_attempts;
+
+def add_to_score():
+    round_score = 6 - num_of_attempts()
+    return round_score;
+    
+def final_score(username, score):
+    if score < 10:
+        score = "{0.0=2d}".format(score)
+    with open("data/scores.txt", "a") as file:
+        file.writelines(str(username) + "\n")
+        file.writelines(score)
+        
+def get_scores():
+    usernames = []
+    scores = []
+    
+    with open("data/scores.txt", "r") as file:
+        lines = file.read().splitlines()
+        
+    for i, text in enumerate(lines):
+        if i%2 ==0:
+            scores.append(text)
+        else:
+            usernames.append(text)
+    
+    usernames_and_scores = zip(usernames, scores)
+    sorted(usernames_and_scores, key=lambda x: x[1])
+    """usernames_and_scores.sort(key=itemgetter(1), reverse=True)"""
+    return usernames_and_scores
 
 @app.route('/', methods=["GET", "POST"])
 def index():
+    clear_answers()
+    score = 0
     """Handle POST request"""
     if request.method == "POST":
         write_to_file("data/users.txt", request.form["username"] + "\n")
-        return redirect(request.form["username"])
+        username = request.form["username"]
+        return redirect(url_for('user', username=username, score=score))
     return render_template("index.html")
 
-@app.route('/<username>', methods=["GET", "POST"])
-def user(username):
-    attempts = get_all_attempts()
-    attempts_num = len(attempts)
+@app.route('/<username>/riddle1/<int:score>', methods=["GET", "POST"])
+def user(username, score):
+    rem_attempts = 5
     if request.method == "POST":
         write_to_file("data/guesses.txt", request.form["answer"] + "\n")
         guess = request.form["answer"]
         answers = riddle_answers()
-        username=username
         if answers[0] == guess:
-            score = 5 - attempts_num
+            score = score + add_to_score()
             clear_answers()
-            return redirect(url_for('riddle2', username=username))
+            return redirect(url_for('riddle2', username=username, score=score))
         else:
-            if attempts_num > 4:
-                return redirect(url_for('riddle2', username=username))
+            if num_of_attempts() > 4:
+                clear_answers()
+                return redirect(url_for('riddle2', username=username, score=score))
             else:
-                return render_template("riddle1.html", username=username, attempts=attempts, attempts_num=attempts_num)
-    return render_template("riddle1.html", username=username)
+                return render_template("riddle1.html", username=username, attempts=get_all_attempts(), rem_attempts=attempts_rem(), score=score)
+    return render_template("riddle1.html", username=username, rem_attempts=rem_attempts, score=score)
 
-@app.route('/<username>/riddle2', methods=["GET", "POST"])
-def riddle2(username):
+@app.route('/<username>/riddle2/<int:score>', methods=["GET", "POST"])
+def riddle2(username, score):
+    rem_attempts = 5
     if request.method == "POST":
         write_to_file("data/guesses.txt", request.form["answer"] + "\n")
         guess = request.form["answer"]
         answers = riddle_answers()
         if answers[1] == guess:
+            score = score + add_to_score()
             clear_answers()
-            return redirect(url_for('riddle3', username=username)) 
+            return redirect(url_for('riddle3', username=username, score=score))
         else:
-            return render_template("riddle2.html", username=username)    
-    return render_template("riddle2.html", username=username)  
+            if num_of_attempts() > 4:
+                clear_answers()
+                return redirect(url_for('riddle3', username=username, score=score))
+            else:
+                return render_template("riddle2.html", username=username, attempts=get_all_attempts(), rem_attempts=attempts_rem(), score=score)
+    return render_template("riddle2.html", username=username, rem_attempts=rem_attempts, score=score)
 
-@app.route('/<username>/riddle3', methods=["GET", "POST"])    
-def riddle3(username):
+@app.route('/<username>/riddle3/<int:score>', methods=["GET", "POST"])
+def riddle3(username, score):
+    rem_attempts = 5
     if request.method == "POST":
         write_to_file("data/guesses.txt", request.form["answer"] + "\n")
         guess = request.form["answer"]
         answers = riddle_answers()
         if answers[2] == guess:
+            score = score + add_to_score()
             clear_answers()
-            return redirect(url_for('riddle4', username=username))
+            return redirect(url_for('riddle4', username=username, score=score))
         else:
-            return render_template("riddle3.html", username=username)
-    return render_template("riddle3.html", username=username)
-    
-@app.route('/<username>/riddle4', methods=["GET", "POST"])    
-def riddle4(username):
+            if num_of_attempts() > 4:
+                clear_answers()
+                return redirect(url_for('riddle4', username=username, score=score))
+            else:
+                return render_template("riddle3.html", username=username, attempts=get_all_attempts(), rem_attempts=attempts_rem(), score=score)
+    return render_template("riddle3.html", username=username, rem_attempts=rem_attempts, score=score)
+
+@app.route('/<username>/riddle4/<int:score>', methods=["GET", "POST"])
+def riddle4(username, score):
+    rem_attempts = 5
     if request.method == "POST":
         write_to_file("data/guesses.txt", request.form["answer"] + "\n")
         guess = request.form["answer"]
         answers = riddle_answers()
         if answers[3] == guess:
+            score = score + add_to_score()
             clear_answers()
-            return redirect(url_for('riddle5', username=username))
+            return redirect(url_for('riddle5', username=username, score=score))
         else:
-            return render_template("riddle4.html", username=username)
-    return render_template("riddle5.html", username=username)
+            if num_of_attempts() > 4:
+                clear_answers()
+                return redirect(url_for('riddle5', username=username, score=score))
+            else:
+                return render_template("riddle4.html", username=username, attempts=get_all_attempts(), rem_attempts=attempts_rem(), score=score)
+    return render_template("riddle4.html", username=username, rem_attempts=rem_attempts, score=score)
     
-@app.route('/<username>/riddle5', methods=["GET", "POST"])    
-def riddle5(username):
+@app.route('/<username>/riddle5/<int:score>', methods=["GET", "POST"])
+def riddle5(username, score):
+    rem_attempts = 5
     if request.method == "POST":
         write_to_file("data/guesses.txt", request.form["answer"] + "\n")
         guess = request.form["answer"]
         answers = riddle_answers()
         if answers[4] == guess:
+            score = score + add_to_score()
             clear_answers()
-            return render_template("riddle6.html", username=username)
+            final_score(score, username)
+            return redirect(url_for('scoreboard'))
         else:
-            return render_template("riddle3.html", username=username)
-    return render_template("riddle3.html", username=username)
+            if num_of_attempts() > 4:
+                clear_answers()
+                final_score(score, username)
+                return redirect(url_for('scoreboard'))
+            else:
+                return render_template("riddle5.html", username=username, attempts=get_all_attempts(), rem_attempts=attempts_rem(), score=score)
+    return render_template("riddle5.html", username=username, rem_attempts=rem_attempts, score=score)
+
     
 @app.route('/scoreboard')
 def scoreboard():
-    return render_template("scoreboard.html")
+    usernames_and_scores = get_scores()
+    return render_template("scoreboard.html", usernames_and_scores=usernames_and_scores)
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
