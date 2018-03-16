1060b6e1d1e27e06824b775195655b8bd236cb92
Dean Flint <dean.p.flint@gmail.com>
2018-03-16 11:46:50 +0000

    Score functionality bug fixed

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 def riddle_answers():
+    """ Create an array of the riddle answers """
     answers = []
     with open("data/answers.txt", "r") as file:
         lines = file.read().splitlines()
@@ -13,15 +14,18 @@ def riddle_answers():
         answers.append(line)
     return answers
 
+
 def clear_answers():
     """ Clear the guesses.txt file"""
     with open("data/guesses.txt", "w"):
         return
+ 
     
 def write_to_file(filename, data):
     """ Handle the process of writing data to a file """
     with open(filename, "a") as file:
         file.writelines(data)
+ 
         
 def get_all_attempts():
     """Get all of the messages and seperate them by a `br` """
@@ -29,25 +33,36 @@ def get_all_attempts():
     with open("data/guesses.txt", "r") as incorrect_attempts:
         attempts = incorrect_attempts.readlines()
     return attempts
+  
     
 def num_of_attempts():
+    """ The number of attempts made by the user on the current riddle """
     attempts = get_all_attempts()
     return len(attempts);
+ 
     
 def attempts_rem():
+    """ Return the number of attempts remaining """
     rem_attempts = 5 - num_of_attempts()
     return rem_attempts;
 
+
 def add_to_score():
+    """ Add the score award during the round depeneding on how many turns it took to answer correctly """
     round_score = 6 - num_of_attempts()
     return round_score;
+  
     
 def final_score(username, score):
-    if score < 10:
-        score = "{0.0=2d}".format(score)
-    with open("data/scores.txt", "a") as file:
-        file.writelines(str(username) + "\n")
-        file.writelines(score)
+    if username != "" and score != "":
+        with open("data/scores.txt", "a") as file:
+            if int(score) > 0 and int(score) < 10:
+                score = "0" + str(score)
+            file.writelines(str(score) + "\n")
+            file.writelines(str(username) + "\n")
+    else:
+        return
+  
         
 def get_scores():
     usernames = []
@@ -63,9 +78,9 @@ def get_scores():
             usernames.append(text)
     
     usernames_and_scores = zip(usernames, scores)
-    sorted(usernames_and_scores, key=lambda x: x[1])
-    """usernames_and_scores.sort(key=itemgetter(1), reverse=True)"""
+    usernames_and_scores.sort(key=itemgetter(1), reverse=True)
     return usernames_and_scores
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -77,6 +92,7 @@ def index():
         username = request.form["username"]
         return redirect(url_for('user', username=username, score=score))
     return render_template("index.html")
+
 
 @app.route('/<username>/riddle1/<int:score>', methods=["GET", "POST"])
 def user(username, score):
@@ -97,6 +113,7 @@ def user(username, score):
                 return render_template("riddle1.html", username=username, attempts=get_all_attempts(), rem_attempts=attempts_rem(), score=score)
     return render_template("riddle1.html", username=username, rem_attempts=rem_attempts, score=score)
 
+
 @app.route('/<username>/riddle2/<int:score>', methods=["GET", "POST"])
 def riddle2(username, score):
     rem_attempts = 5
@@ -115,6 +132,7 @@ def riddle2(username, score):
             else:
                 return render_template("riddle2.html", username=username, attempts=get_all_attempts(), rem_attempts=attempts_rem(), score=score)
     return render_template("riddle2.html", username=username, rem_attempts=rem_attempts, score=score)
+
 
 @app.route('/<username>/riddle3/<int:score>', methods=["GET", "POST"])
 def riddle3(username, score):
@@ -135,6 +153,7 @@ def riddle3(username, score):
                 return render_template("riddle3.html", username=username, attempts=get_all_attempts(), rem_attempts=attempts_rem(), score=score)
     return render_template("riddle3.html", username=username, rem_attempts=rem_attempts, score=score)
 
+
 @app.route('/<username>/riddle4/<int:score>', methods=["GET", "POST"])
 def riddle4(username, score):
     rem_attempts = 5
@@ -153,6 +172,7 @@ def riddle4(username, score):
             else:
                 return render_template("riddle4.html", username=username, attempts=get_all_attempts(), rem_attempts=attempts_rem(), score=score)
     return render_template("riddle4.html", username=username, rem_attempts=rem_attempts, score=score)
+ 
     
 @app.route('/<username>/riddle5/<int:score>', methods=["GET", "POST"])
 def riddle5(username, score):
@@ -164,17 +184,17 @@ def riddle5(username, score):
         if answers[4] == guess:
             score = score + add_to_score()
             clear_answers()
-            final_score(score, username)
+            final_score(username, score)
             return redirect(url_for('scoreboard'))
         else:
             if num_of_attempts() > 4:
                 clear_answers()
-                final_score(score, username)
+                final_score(username, score)
                 return redirect(url_for('scoreboard'))
             else:
                 return render_template("riddle5.html", username=username, attempts=get_all_attempts(), rem_attempts=attempts_rem(), score=score)
     return render_template("riddle5.html", username=username, rem_attempts=rem_attempts, score=score)
-
+    
     
 @app.route('/scoreboard')
 def scoreboard():
